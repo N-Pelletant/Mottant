a70f27aa8b1832a4be103e6a14f458e9b70f6e4c
N-Pelletant <pelletantnic@gmail.com>
2022-12-16 19:01:04 +0100

    feat(): change timer to minute

--- src/hooks/useDailyWord.ts
+++ src/hooks/useDailyWord.ts
@@ -1,20 +1,20 @@
-import { useMemo } from 'react';
-import words from '../words';
+import { useMemo } from "react";
+import words from "../words";
 
 const mulberry32 = (seed: number) => {
-  let t = seed += 0x6D2B79F5;
-  t = Math.imul(t ^ t >>> 15, t | 1);
-  t ^= t + Math.imul(t ^ t >>> 7, t | 61);
-  return ((t ^ t >>> 14) >>> 0) / 4294967296;
-}
+  let t = (seed += 0x6d2b79f5);
+  t = Math.imul(t ^ (t >>> 15), t | 1);
+  t ^= t + Math.imul(t ^ (t >>> 7), t | 61);
+  return ((t ^ (t >>> 14)) >>> 0) / 4294967296;
+};
 
 const useDailyWord = () => {
   const word = useMemo(() => {
-    const seed = Math.floor(new Date().getTime() / 86400000) * 86400000;
-    return words[Math.floor(mulberry32(seed) * words.length)]
-  }, [])
+    const seed = Math.floor(new Date().getTime() / 1000) * 1000;
+    return words[Math.floor(mulberry32(seed) * words.length)];
+  }, []);
 
   return word;
-}
+};
 
 export default useDailyWord;
